9ebdbf9265b18def8074b10d977268b18d01da0d
Melissa He <melissahe@ac.c4q.nyc>
2017-11-02 18:32:48 -0400

    Add different sections for different classifications

--- AC-iOS-Multiple-MVC/AC-iOS-Multiple-MVC/View Controllers/ViewController.swift
+++ AC-iOS-Multiple-MVC/AC-iOS-Multiple-MVC/View Controllers/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBOutlet weak var zooAnimalTableView: UITableView!
     
-    var zooAnimals: [ZooAnimal] = ZooAnimal.zooAnimals
+    var zooAnimals: [[ZooAnimal]] = [[]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,17 +20,44 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         zooAnimalTableView.dataSource = self
         zooAnimalTableView.rowHeight = UITableViewAutomaticDimension
         zooAnimalTableView.estimatedRowHeight = 300
+        zooAnimals[0] = ZooAnimal.zooAnimals.filter{$0.classification == "Mammal"}
+        zooAnimals.append(ZooAnimal.zooAnimals.filter{$0.classification == "Amphibian"})
+        zooAnimals.append(ZooAnimal.zooAnimals.filter{$0.classification == "Reptile"})
+        zooAnimals.append(ZooAnimal.zooAnimals.filter{$0.classification == "Insect"})
+        zooAnimals.append(ZooAnimal.zooAnimals.filter{$0.classification == "Bird"})
     }
     
     //Data Source Methods
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return zooAnimals.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Mammal"
+        case 1:
+            return "Amphibian"
+        case 2:
+            return "Reptile"
+        case 3:
+            return "Insect"
+        case 4:
+            return "Bird"
+        default:
+            return nil
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return zooAnimals[section].count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Zoo Animal Cell", for: indexPath)
+        let sectionToSetUp = indexPath.section
         let rowToSetUp = indexPath.row
-        let currentZooAnimal = zooAnimals[rowToSetUp]
+        let currentZooAnimal = zooAnimals[sectionToSetUp][rowToSetUp]
         
         if let zooAnimalCell = cell as? ZooAnimalTableViewCell {
             zooAnimalCell.animalNameLabel.text = currentZooAnimal.name
@@ -49,9 +76,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             //specify the segue identifier, then destination VC
             if segue.identifier == "Zoo Animal Detailed Segue", let destinationVC = segue.destination as? ZooAnimalDetailedViewController {
                 //find specific indexPath (so you can find the section and row) of the sender cell
-                let cellIndexPath = zooAnimalTableView.indexPath(for: zooAnimalCell)
+                let cellIndexPath = zooAnimalTableView.indexPath(for: zooAnimalCell)!
                 //rows and indices are zero-index, so we can use the rows as the datasource index - row of the current sender = same location in the array
-                let selectedZooAnimalCell = zooAnimals[cellIndexPath!.row]
+                let selectedZooAnimalCell = zooAnimals[cellIndexPath.section][cellIndexPath.row]
                 destinationVC.zooAnimal = selectedZooAnimalCell
             }
         }
